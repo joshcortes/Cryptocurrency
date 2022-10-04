fc7592884edf681135cdc2d5317caca1be717907
joshcortes <joshuacortes28@gmail.com>
2022-10-04 19:09:40 -0400

    first commit

---
+++ cryptoapp/src/App.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import Coins from './components/Coins';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  const [coins, setCoins] = useState([])
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+  useEffect(() =>{
+    axios.get(url).then( (response) => {
+      setCoins(response.data)
+      console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+  return (
+    <>
+      <Navbar />
+      <Coins coins ={coins} />
+
+      
+    </>
+  );
+}
+
+export default App;
